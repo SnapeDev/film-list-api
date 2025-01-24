781289bd9df168bcaf8eb99e53f83dbf89c4a540
Jack <jacksnape89@gmail.com>
2025-01-24 09:54:19 +0000

    latest

--- app.js
+++ app.js
@@ -5,13 +5,9 @@ const PORT = process.env.PORT || 3001;
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 const User = require("./schemas/User");
-const { v4: uuidv4 } = require("uuid");
+// const { v4: uuidv4 } = require("uuid");
 const cors = require("cors");
-app.use(
-  cors({
-    origin: "http://localhost:3000",
-  })
-);
+app.use(cors());
 
 dotenv.config();
 connectDB();
@@ -19,30 +15,30 @@ connectDB();
 // Middleware
 app.use(express.json());
 
-app.post("/movies/auth", async (req, res) => {
-  console.log("arrived");
-  const user = await User.findOne({ username: req.body.username });
-  if (!user) {
-    return res.sendStatus(401);
-  }
-  if (req.body.password !== user.password) {
-    return res.sendStatus(403);
-  }
-  user.token = uuidv4();
-  await user.save();
-  res.send({ token: user.token });
-});
+// app.post("/movies/auth", async (req, res) => {
+//   console.log("arrived");
+//   const user = await User.findOne({ username: req.body.username });
+//   if (!user) {
+//     return res.sendStatus(401);
+//   }
+//   if (req.body.password !== user.password) {
+//     return res.sendStatus(403);
+//   }
+//   user.token = uuidv4();
+//   await user.save();
+//   res.send({ token: user.token });
+// });
 
-// Authorization middleware
-app.use(async (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  const user = await User.findOne({ token: authHeader });
-  if (user) {
-    next();
-  } else {
-    res.sendStatus(403);
-  }
-});
+// // Authorization middleware
+// app.use(async (req, res, next) => {
+//   const authHeader = req.headers["authorization"];
+//   const user = await User.findOne({ token: authHeader });
+//   if (user) {
+//     next();
+//   } else {
+//     res.sendStatus(403);
+//   }
+// });
 
 // Use the movies router
 app.use("/movies", moviesRouter);
